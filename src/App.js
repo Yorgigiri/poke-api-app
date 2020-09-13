e280cbb8760ebd1500931c5652bab4b26a7dcc70
Yorgigiri <yorgigiri@yandex.ru>
2020-09-13 23:33:05 +0400

    add some functions to get info about each pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from "./services/pokemon";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getAllPokemon(initialUrl)
+      console.log(response);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    const _pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        const pokemonRecord = await getPokemon(pokemon.url);
+        // const pokemonRecord = pokemon.url;
+        // console.log('pokemonRecord: ', pokemonRecord);
+
+
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData);
+  };
+
+  console.log();
+  console.log(pokemonData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        { loading ? <h1>Loadind...</h1> : (
+          <h1>Data is fetched </h1>
+        )}
+      </div>
     </div>
   );
 }
