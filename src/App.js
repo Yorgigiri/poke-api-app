83aedab12cf7bdd2dc9733e9f36dfb7e26f21c35
Grigory <yorgigiri@yandex.ru>
2020-10-14 02:00:47 +0400

    stylize pokemon card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from "./services/pokemon";
-import Card from "./components";
+import Card from "./components/Card";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Button } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  app: {
+    minHeight: '100vh',
+    backgroundColor: '#f5f5f5',
+  },
+  mainContainer: {
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  buttonWrapper: {
+    padding: '16px 0',
+  },
+  button: {
+    margin: 8,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [pokemonData, setPokemonData] = useState([]);
   const [nextUrl, setNextUrl] = useState('');
   const [prevUrl, setPrevUrl] = useState('');
@@ -12,35 +38,87 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await getAllPokemon(initialUrl)
-      console.log(response);
+      const response = await getAllPokemon(initialUrl);
+      console.log('response: ', response);
       setNextUrl(response.next);
       setPrevUrl(response.previous);
       await loadPokemon(response.results);
       setLoading(false);
     }
+
     fetchData();
   }, []);
 
   const loadPokemon = async (data) => {
+    console.log('data: ', data);
+
     const _pokemonData = await Promise.all(
-      data.map(async ({ url }) => await getPokemon(url))
+        data.map(async ({ url }) => await getPokemon(url))
     )
 
     setPokemonData(_pokemonData);
   };
 
+  const next = async () => {
+    if (!nextUrl) return;
+    setLoading(true);
+    const data = await getAllPokemon(nextUrl);
+    console.log('data!!!: ', data);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false)
+  }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    const data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false)
+  }
+
   console.log('pokemonData: ', pokemonData);
 
   return (
-    <div className="App">
-      { loading ? <h1>Loadind...</h1> : (
-        <>
-          {pokemonData.map((pokemon, i) => {
-            return <Card key={i} pokemon={pokemon} />;
-          })}
-        </>
-      )}
+    <div className={classes.app}>
+        <Container className={classes.mainContainer}>
+          {loading
+              ? <h1>Loading...</h1>
+              : (
+                <>
+                  <h1>Pokemon grid</h1>
+                  <Grid container justify={"center"} className={classes.buttonWrapper}>
+                    <Button onClick={prev} variant="contained" color="primary">
+                      Prev
+                    </Button>
+                    <Button onClick={next} variant="contained" color="primary">
+                      Next
+                    </Button>
+                    {/*<Button variant="contained" color="primary">*/}
+                    {/*  Load More*/}
+                    {/*</Button>*/}
+                  </Grid>
+                  <Grid container className={classes.root} spacing={2}>
+                    {pokemonData.map((pokemon, i) => <Card key={i} pokemon={pokemon} />)}
+                  </Grid>
+                  <Grid container justify={"center"} className={classes.buttonWrapper}>
+                    <Button onClick={prev} variant="contained" color="primary" className={classes.button}>
+                      Prev
+                    </Button>
+                    <Button onClick={next} variant="contained" color="primary" className={classes.button}>
+                      Next
+                    </Button>
+                    {/*<Button variant="contained" color="primary">*/}
+                    {/*  Load More*/}
+                    {/*</Button>*/}
+                  </Grid>
+                </>
+              )
+          }
+        </Container>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import { List } from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: 8,
+    },
+    cardImage: {
+        textAlign: "center",
+        padding: '10px 0',
+    },
+    cardName: {
+        textAlign: "center",
+        fontSize: 16,
+        marginBottom: 16,
+    },
+}));
+
+const Card = ({ pokemon }) => {
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={12} sm={6} md={3}>
+            <Paper className={classes.paper}>
+                <div className={classes.cardImage}>
+                  <img src={pokemon.sprites.front_default} alt=""/>
+                </div>
+                <Divider />
+                <Typography variant="subtitle1" className={classes.cardName}>
+                    {pokemon.name}
+                </Typography>
+                <div>
+                    <Typography variant="subtitle2">Types:</Typography>
+                    <List>
+                        {pokemon.types.map(({ type }) => {
+                            return (
+                                <ListItem xs={"auto"}>{`${type.name}`}</ListItem>
+                            )
+                        })}
+                    </List>
+                </div>
+                <Divider />
+                <div>
+                    <Typography variant="subtitle2">Weight:</Typography>
+                    {pokemon.weight}
+                </div>
+                <div>
+                  <Typography variant="subtitle2">Height:</Typography>
+                  {pokemon.height}
+                </div>
+                <div>
+                    <Typography variant="subtitle2">Abilities:</Typography>
+                    <List>
+                        {pokemon.abilities.map(({ ability }) => {
+                            return (
+                                <ListItem xs={"auto"}>{`${ability.name}`}</ListItem>
+                            )
+                        })}
+                    </List>
+                </div>
+            </Paper>
+        </Grid>
+    );
+}
+
+export default Card;
